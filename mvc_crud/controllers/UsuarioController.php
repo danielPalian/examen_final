b09ddd310504d0dd3edf59cb45c28d26caf85a2c
palian <cpalianyu@ucvvirtual.edu.pe>
2024-12-05 19:49:51 -0500

    mi primer commit

---
+++ mvc_crud/controllers/UsuarioController.php
@@ -0,0 +1,31 @@
+<?php
+require_once "models/Usuario.php";
+
+class UsuarioController {
+    private $usuarioModel;
+
+    public function __construct() {
+        $this->usuarioModel = new Usuario();
+    }
+
+    public function index() {
+        $usuarios = $this->usuarioModel->obtenerUsuarios();
+        include "views/usuarios/index.php";
+    }
+
+    public function crear($nombre_usuario, $contraseña, $id_perfil) {
+        if ($this->usuarioModel->crearUsuario($nombre_usuario, $contraseña, $id_perfil)) {
+            header("Location: index.php");
+        } else {
+            echo "Error al crear el usuario.";
+        }
+    }
+
+    public function eliminar($id_usuario) {
+        if ($this->usuarioModel->eliminarUsuario($id_usuario)) {
+            header("Location: index.php");
+        } else {
+            echo "Error al eliminar el usuario.";
+        }
+    }
+}
---
+++ mvc_crud/models/Usuario.php
@@ -0,0 +1,35 @@
+<?php
+require_once "config/database.php";
+
+class Usuario {
+    private $conn;
+    private $table_name = "usuarios";
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function obtenerUsuarios() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function crearUsuario($nombre_usuario, $contraseña, $id_perfil) {
+        $query = "INSERT INTO " . $this->table_name . " (nombre_usuario, contraseña, id_perfil) VALUES (:nombre_usuario, :contraseña, :id_perfil)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":nombre_usuario", $nombre_usuario);
+        $stmt->bindParam(":contraseña", password_hash($contraseña, PASSWORD_BCRYPT));
+        $stmt->bindParam(":id_perfil", $id_perfil);
+        return $stmt->execute();
+    }
+
+    public function eliminarUsuario($id_usuario) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id_usuario = :id_usuario";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id_usuario", $id_usuario);
+        return $stmt->execute();
+    }
+}
